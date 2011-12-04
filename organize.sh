7663a41c455ec304e087437b19d55a76e75a97cc
Mike <mgoeppner@gmail.com>
2011-12-04 06:29:15 -0500

    Added checks to check directory existance

--- organize.sh
+++ organize.sh
@@ -4,56 +4,69 @@
 
 HOMEDIR=~
 
-declare -a target_folders=( $HOMEDIR $HOMEDIR"/Downloads" )
-declare -a sorted_folders=( $HOMEDIR"/Pictures" $HOMEDIR"/Videos" $HOMEDIR"/Music" $HOMEDIR"/Documents" )
+declare -a target_folders=( $HOMEDIR "$HOMEDIR/Downloads" )
+declare -a sorted_folders=( "$HOMEDIR/Pictures" "$HOMEDIR/Videos" "$HOMEDIR/Music" "$HOMEDIR/Documents" )
 
-declare -a sort_type=( 'gif;jpg;jpeg;png;tiff;bmp;svg;psd;xcf' 'mp4;avi' 'mp3;ogg;wav;aac;flac' 'doc;docx;xls;xlsx;ppt;pptx;odt;ods;odp' )
+declare -a sort_type=( 'gif;jpg;jpeg;png;tiff;bmp;svg;psd;xcf' 'mp4;avi' 'mp3;ogg;wav;aac;flac;m4a' 'doc;docx;xls;xlsx;ppt;pptx;odt;ods;odp' )
 
 function sort_folder
 {
-	echo "Sorting " $2
+	echo "Sorting $2"
 	# what is being sorted this pass
 
 	declare -a exts=($(echo $1 | tr ";" "\n"))
 
 	for c in "${exts[@]}"
 	do
-		# Move all the matched files
-		echo "Moving files..."
+		# Get matched files
 		files=($(find $2 -maxdepth 1 -name "*.$c" -type f))
-		echo "${files[@]}"
-		#symlink the files in the orginal directory
+
 		for i in "${files[@]}"
 		do
+			# Move file
 			echo "Moving $i..."
 			mv $i "${sorted_folders[$3]}"
+
+			# Symlink file in original directory
 			echo "Making symlink for $i..."
 			ln -s "${sorted_folders[$3]}"/$(basename $i) $2
 		done
 	done
 }
 
-# change to the current user's home directory
-cd ~
 
-# Lazy, fix this
+# Make sure directories exist
+
+if [ ! -d "$HOMEDIR/Downloads" ]; then
+	mkdir $HOMEDIR/Downloads
+fi
+
+for e in "${sorted_folders[@]}"
+do
+	
+	if [ ! -d "$e" ]; then
+		mkdir $e
+	fi
+
+done
+
+# change to the current user's home directory
+cd $HOMEDIR
+
 ITERATOR=0
 
-# start sorting!
-echo "Sorting now..."
+echo "Sorting started..."
 for t in "${sort_type[@]}"
 do
-	echo "these types: $t"
 
 	for f in "${target_folders[@]}"
 	do
 		echo $f
 		sort_folder $t $f $ITERATOR
-		echo $ITERATOR
 	done
 	ITERATOR=$[ITERATOR+1]
 done
 
-echo "All done!"
+echo "Sorting completed!"
 
 exit 0
