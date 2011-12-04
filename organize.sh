3834e7e41e00460f390506288a9ef3e8a2ecf8c6
Mike <mgoeppner@gmail.com>
2011-12-04 07:06:24 -0500

    Refined error checking

--- organize.sh
+++ organize.sh
@@ -4,10 +4,27 @@
 
 HOMEDIR=~
 
+ERR_XDG="XDG directories do not exist and could not be created, please create them and try again"
+ERR_SORT_MOVE="File could not be moved, operation aborted"
+ERR_SORT_LINK="File could not be symlinked, operation aborted"
+
+# Folders to organize
 declare -a target_folders=( $HOMEDIR "$HOMEDIR/Downloads" )
+
+# Folders to sort to
 declare -a sorted_folders=( "$HOMEDIR/Pictures" "$HOMEDIR/Videos" "$HOMEDIR/Music" "$HOMEDIR/Documents" )
 
+# File types to sort
 declare -a sort_type=( 'gif;jpg;jpeg;png;tiff;bmp;svg;psd;xcf' 'mp4;avi' 'mp3;ogg;wav;aac;flac;m4a' 'doc;docx;xls;xlsx;ppt;pptx;odt;ods;odp' )
+
+# Check if command was successful
+function check_sanity
+{
+	if [ ! "$?" = "0" ]; then
+		echo $1		
+		exit 1		
+	fi	
+}
 
 function sort_folder
 {
@@ -25,10 +42,12 @@ function sort_folder
 			# Move file
 			echo "Moving $i..."
 			mv $i "${sorted_folders[$3]}"
+			check_sanity $ERR_SORT_MOVE
 
 			# Symlink file in original directory
 			echo "Making symlink for $i..."
 			ln -s "${sorted_folders[$3]}"/$(basename $i) $2
+			check_sanity $ERR_SORT_LINK
 		done
 	done
 }
@@ -38,6 +57,7 @@ function sort_folder
 
 if [ ! -d "$HOMEDIR/Downloads" ]; then
 	mkdir $HOMEDIR/Downloads
+
 fi
 
 for e in "${sorted_folders[@]}"
@@ -45,6 +65,9 @@ do
 	
 	if [ ! -d "$e" ]; then
 		mkdir $e
+		if [ ! "$?" = "0" ]; then
+			check_sanity $ERR_XDG					
+		fi
 	fi
 
 done
@@ -52,7 +75,8 @@ done
 # change to the current user's home directory
 cd $HOMEDIR
 
-echo "Sorting started..."
+echo "Orginization started..."
+
 for f in "${target_folders[@]}"
 do
 	ITERATOR=0
@@ -64,6 +88,6 @@ do
 	echo $f
 done
 
-echo "Sorting completed!"
+echo "Orginization completed!"
 
 exit 0
